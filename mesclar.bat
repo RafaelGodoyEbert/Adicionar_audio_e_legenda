90d769a614bdbd1b3b32f651e2f990e8dcc0f537
Rafael Godoy <78083427+RafaelGodoyEbert@users.noreply.github.com>
2024-03-14 19:38:49 -0300

    Conversão .wav para .mp3
    Como no trabalho que faço mantemos o backup, optamos por deixar em .mp3 para que fique menor.
    Então caso na sua pasta audios tenha algum .wav, após mesclar ele converte para .mp3

--- mesclar.bat
+++ mesclar.bat
@@ -10,13 +10,15 @@ for %%a in (audios\*.mp3) do (
     set "input=%%a"
     set "output=!output_folder!\!input:audios\=!"
     set "output=!output:.mp3=(com background).mp3!"
-    ffmpeg -i "%background%" -i "!input!" -filter_complex "[0:a] [1:a] amix=inputs=2:duration=longest" "!output!" -y
+    ffmpeg -i "%background%" -i "!input!" -filter_complex "[1:a]volume=2[a];[0:a][a]amix=inputs=2:duration=longest" "!output!" -y
 )
 
 for %%a in (audios\*.wav) do (
     set "input=%%a"
     set "output=!output_folder!\!input:audios\=!"
     set "output=!output:.wav=(com background).wav!"
-    ffmpeg -i "%background%" -i "!input!" -filter_complex "[0:a] [1:a] amix=inputs=2:duration=longest" "!output!" -y
+    ffmpeg -i "%background%" -i "!input!" -filter_complex "[1:a]volume=2[a];[0:a][a]amix=inputs=2:duration=longest" "!output!" -y
+    ffmpeg -i "!output!" -codec:a libmp3lame "!output:.wav=.mp3!" -y
+    del "!output!"
 )
 pause
